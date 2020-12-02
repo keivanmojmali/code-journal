ebd4c19616053e3b561e4e6dd6cec0dfefe290a9
Keivan Mojmali <mojmalik@gmail.com>
2020-12-02 13:25:59 -0800

    issue one resolved

--- js/data.js
+++ js/data.js
@@ -12,28 +12,12 @@ var data = {
   entries: []
 };
 
-var userEntries = [];
+var userEntries = {};
 var $savedProfiles = localStorage.getItem('user-profile');
-userEntries = JSON.parse($savedProfiles);
-var $contact = document.querySelector('#userForm');
-var $imgPlace = document.querySelector('#placeholder');
+userEntries = JSON.parse($savedProfiles)
 
 window.addEventListener('beforeunload', function (e) {
   var $profiles = JSON.stringify(userEntries);
   localStorage.setItem('user-profile', $profiles);
-
-});
-
-document.addEventListener('submit', function (e) {
-  e.preventDefault();
-  var url = $contact.elements.url.value;
-  var username = $contact.elements.username.value;
-  var fullName = $contact.elements.fullName.value;
-  var location = $contact.elements.location.value;
-  var bio = $contact.elements.bio.value;
-  var values = { username, fullName, location, url, bio };
-  userEntries.push(values);
-  $contact.reset();
-  $imgPlace.src = './images/placeholder-image-square.jpg';
 
 });
--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 var $avatarUrl = document.querySelector('#url');
 var $imgPlace = document.querySelector('#placeholder');
+var $contact = document.querySelector('#userForm');
+
 
 $avatarUrl.addEventListener('input', function (e) {
   var $newUrl = e.target.value;
   $imgPlace.src = $newUrl;
+});
+
+
+document.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var url = $contact.elements.url.value;
+  var username = $contact.elements.username.value;
+  var fullName = $contact.elements.fullName.value;
+  var location = $contact.elements.location.value;
+  var bio = $contact.elements.bio.value;
+  var values = { username, fullName, location, url, bio };
+  userEntries = values;
+  $contact.reset();
+  $imgPlace.src = './images/placeholder-image-square.jpg';
+
 });
